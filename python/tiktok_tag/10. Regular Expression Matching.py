df087e76c67e007d4d53a6b34ebb277bf9173f61
jieyaochen <chenjieyao0824@gmail.com>
2025-04-05 18:31:05 -0400

    jiayou

---
+++ python/tiktok_tag/10. Regular Expression Matching.py
@@ -0,0 +1,28 @@
+class Solution:
+    #top down memo dfs
+    #	•	Time Complexity: O(m * n)
+	#•	Space Complexity: O(m * n)
+    def isMatch(self, s: str, p: str) -> bool:
+        cache = {}
+
+        def dfs(i:int, j:int):
+            if (i,j) in cache:
+                return cache[(i,j)]
+            if i >= len(s) and j>= len(p):
+                return True
+
+            if j >= len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            if j + 1 < len(p) and p[j + 1] == '*':
+                cache[(i,j)] = dfs(i,j + 2) or (match and dfs(i + 1,j))# use *,i match
+                return cache[(i,j)]
+
+            if match:
+                cache[(i,j)] = dfs(i + 1,j + 1)
+                return cache[(i,j)]
+            cache[(i,j)] = False
+            return False
+
+        return dfs(0,0)
