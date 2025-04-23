46403b25ab8a862fb033ea3f2ca4591ceb64e4f9
jieyaochen <chenjieyao0824@gmail.com>
2025-04-23 14:49:22 -0400

    tt

--- python/tiktok_tag/200. Number of Islands.py
+++ python/tiktok_tag/200. Number of Islands.py
@@ -43,3 +43,37 @@ class Solution:
         self.dfs(grid,i - 1, j)
         self.dfs(grid, i, j+ 1)
         self.dfs(grid, i, j- 1)
+
+
+''''#bfs version''':
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        res = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    res += 1
+                    self.bfs(grid,i,j)
+        return res
+
+
+
+    def bfs(self, grid,i,j):
+        m = len(grid)
+        n = len(grid[0])
+
+        queue = deque()
+        queue.append((i,j))
+        grid[i][j] = '#'  # 标记为已访问
+
+        while queue:
+            x,y = queue.popleft()
+            directions = [(1,0),(0,1),(-1,0),(0,-1)]
+            for dx,dy in directions:
+                dx,dy = x+dx,y+dy
+                if 0 <= dx < m and 0 <= dy < n and grid[dx][dy] == '1':
+                    grid[dx][dy] = '#'  # 标记已访问
+                    queue.append((dx, dy))
