c5af1e9fb99976d39e1fb707d95787b53511e78f
jieyaochen <chenjieyao0824@gmail.com>
2025-08-19 16:06:22 -0400

    mark

--- python/binary_search/744. Find Smallest Letter Greater Than Target.py
+++ python/binary_search/744. Find Smallest Letter Greater Than Target.py
@@ -1,20 +1,25 @@
 class Solution:
-    #time:logn
-    #space:O1
+    '''
+    time:Logn
+    space:O1
+    #main idea:在升序排列的字符数组 letters 中，找出严格大于 target 的最小字母。如果找不到（也就是 target ≥ 所有字母），就返回第一个字母（wrap around）。
+    '''
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        if not letters:
+            return None
         left = 0
         right = len(letters) - 1
-        
-        # 如果目标字符比数组最大字符还大，或者等于最大字符，需要返回第一个字符，因为letters本来就是升序的
+
         if target >= letters[-1]:
             return letters[0]
-            
+
         while left <= right:
             mid = left + (right - left) // 2
-            
+
             if letters[mid] <= target:
                 left = mid + 1
-            elif letters[mid] > target:  # letters[mid] > target
+
+            elif letters[mid] > target:
                 right = mid - 1
-        
+
         return letters[left]
